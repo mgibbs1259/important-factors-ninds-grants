a9576b9f799af13e5e6120475da46be6a659abbf
Mary <mgibbs1259@gmail.com>
2019-02-15 18:25:10 -0500

    scopus

--- scopus_miner.py
+++ scopus_miner.py
@@ -1,3 +1,21 @@
 import scopus
+from scopus import AuthorSearch
+from scopus import AuthorRetrieval
+import pandas as pd
 
+s = AuthorSearch('AUTHLAST(Han) and AUTHFIRST(Chun)', refresh = True)
+df = (pd.DataFrame(s.authors))
 
+df = df[df['affiliation'] == 'Cornell University']
+
+print(df)
+
+au = AuthorRetrieval(df.loc['affiliation_id'])
+print(au.citation_count)
+print(au.document_count)
+print(au.h_index)
+print(au.publication_range)
+print(pd.DataFrame(au.journal_history).head())
+print(au.get_coauthors)
+
+print(df)
