66b25e20fe243bac957482e5db0730ae6d2d5380
dvitale199 <d.vitale199@gmail.com>
2019-02-15 16:29:02 -0500

    scopus

--- scopus_miner.py
+++ scopus_miner.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 driver = webdriver.Chrome()
 
 driver.get("https://www.scopus.com/freelookup/form/author.uri?zone=&origin=NO%20ORIGIN%20DEFINED")
-driver.sleep(2)
+driver.(2)
 driver.find_element_by_css_selector(".formControls > .inputTextLabel").click()
 drive.find_element_by_css_selector("#lastname").send_keys("Han")
 # driver.close()
