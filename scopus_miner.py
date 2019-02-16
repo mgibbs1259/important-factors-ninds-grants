fd0d18de34264a63a4561de98f8b50c1f1da3c02
Mary <mgibbs1259@gmail.com>
2019-02-16 14:06:24 -0500

    series of info from scopus

--- scopus_miner.py
+++ scopus_miner.py
@@ -12,37 +12,27 @@ df = pd.DataFrame(scopus_authors.authors)
 df = df[df['affiliation'] == 'Cornell University']
 
 
-# Parse eid to get Scopus ID
+# Get Scopus ID
 eid = df["eid"].values
 scopus_id = eid[0][7:]
-
-
 # Use AuthorRetrieval
 au = AuthorRetrieval(scopus_id)
-scopus_id = {'scopus_id': scopus_id}
-document_count = {'document_count': au.document_count}
-citation_count = {'citation_count': au.citation_count}
-h_index = {'h_index': au.h_index}
-begin_publication_range = {'begin_publication_range': au.publication_range[0]}
-end_publication_range = {'end_publication_range': au.publication_range[1]}
 
 
-# Use affiliation IDs to get institution name
+# Get Current Affiliation ID
 aff_current_ID = au.affiliation_current
-
-
 # Use ContentAffiliationRetrieval
 aff_current = ContentAffiliationRetrieval(aff_current_ID)
-aff_current_name = {'aff_current_name': aff_current.affiliation_name}
-aff_current_city = {'aff_current_city': aff_current.city}
-aff_current_state = {'aff_current_state': aff_current.state}
-aff_current_auth_count = {'aff_current_auth_count': aff_current.author_count}
-aff_current_doc_count = {'aff_current_doc_count': aff_current.document_count}
 
 
-# Create dataframe
-df = pd.DataFrame.from_dict([scopus_id, document_count, citation_count, h_index, begin_publication_range,
-                             end_publication_range, aff_current_name, aff_current_city, aff_current_state,
-                             aff_current_auth_count, aff_current_doc_count])
+# Create Scopus dictionary
+scopus_dict = {'name': au.given_name + ' ' + au.surname, 'scopus_id': scopus_id,
+               'document_count': au.document_count, 'citation_count': au.citation_count,
+               'h_index': au.h_index, 'begin_publication_range': au.publication_range[0],
+               'end_publication_range': au.publication_range[1], 'aff_current_name': aff_current.affiliation_name,
+               'aff_current_city': aff_current.city, 'aff_current_state': aff_current.state,
+               'aff_current_auth_count': aff_current.author_count, 'aff_current_doc_count': aff_current.document_count}
 
-print(df)
+# Create Scopus series
+scopus_series = pd.Series(scopus_dict)
+print(scopus_series)
