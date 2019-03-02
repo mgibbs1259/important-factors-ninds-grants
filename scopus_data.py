357fc4d124a26214b1638eb34b1275b83bda2b7c
Mary <mgibbs1259@gmail.com>
2019-03-02 14:15:59 -0500

    rough

--- scopus_data.py
+++ scopus_data.py
@@ -43,7 +43,6 @@ scopus_search = scopus_pi.unique()
 # print(len(scopus_search))
 # 15499 scopus search names
 
-
 # Get Scopus data
 
 scopus_df = pd.DataFrame()
@@ -52,10 +51,9 @@ journals_df = pd.DataFrame()
 affiliations_df = pd.DataFrame()
 
 # Range - 0 to 4900, 4901 to 9800, 9801 to 14700, 14701 to last
-for i in range(0, len(scopus_search.iloc[0:25, :])):
-    
+for i in range(0, 5):
+
     try:
-    
         # Use AuthorSearch
         scopus_authors = AuthorSearch('AUTHLAST(' + scopus_search[i][1] + ') and AUTHFIRST(' + scopus_search[i][0] + ')', refresh = True)
         scopus_authors_df = pd.DataFrame(scopus_authors.authors)
@@ -63,21 +61,20 @@ for i in range(0, len(scopus_search.iloc[0:25, :])):
         print(scopus_author_df)
 
         # Get Scopus ID
-        eid = scopus_author_df['eid'].values
-        scopus_id = eid[0][7:]
-    
+        eid = scopus_author_df['eid']
+        scopus_id = eid[7:]
+
         # Use AuthorRetrieval
         au = AuthorRetrieval(scopus_id)
-
         # Get Current Affiliation ID
         aff_current_ID = au.affiliation_current
-        
+
         # Use ContentAffiliationRetrieval
         aff_current = ContentAffiliationRetrieval(aff_current_ID)
-        
+
         # Create Scopus dictionary
-        scopus_dict = {'scopus idx': scopus_search[i], 'name': scopus_search[i][0].title() + ' ' + scopus_search[i][1].title(),
-                       'scopus_id': scopus_id,'document_count': au.document_count, 'citation_count': au.citation_count,
+        scopus_dict = {'scopus idx': scopus_search[i], 'name': scopus_search[i][0] + ' ' + scopus_search[i][1],
+                       'scopus_id': scopus_id, 'document_count': au.document_count, 'coauthor_count': au.coauthor_count, 'citation_count': au.citation_count,
                        'h_index': au.h_index, 'begin_publication_range': au.publication_range[0],
                        'end_publication_range': au.publication_range[1], 'aff_current_name': aff_current.affiliation_name,
                        'aff_current_city': aff_current.city, 'aff_current_state': aff_current.state,
@@ -85,49 +82,48 @@ for i in range(0, len(scopus_search.iloc[0:25, :])):
 
         # Append scopus dict to scopus df
         scopus_dict_df = pd.DataFrame.from_dict(scopus_dict)
-        scopus_df.append(scopus_dict_df)
-    
+        scopus_df = scopus_df.append(scopus_dict_df)
+
+
         # Get coauthors
         coauth = pd.DataFrame(au.get_coauthors())
-        auth_coauth = [scopus_search[i] for n in range(couth.shape[0] + 1)]
+        auth_coauth = [scopus_search[i] for n in range(coauth.shape[0] + 1)]
         coauth['scopus_idx'] = pd.Series(auth_coauth)
-        
+
         # Append coauthors to coauthors df
-        coauthors_df.append(coauth)
-        
+        coauthors_df = coauthors_df.append(coauth)
+
         # Get journals
         journals = pd.DataFrame(au.get_document_eids(refresh = False))
         auth_journals = [scopus_search[i] for n in range(journals.shape[0] + 1)]
         journals['scopus_idx'] = pd.Series(auth_journals)
-        
+
         # Append coauthors to coauthors df
-        journals_df.append(journals)
-        
+        journals_df = journals_df.append(journals)
+
         # Get former affiliations
         former_aff = au.affiliation_history
         for aff in former_aff:
             aff_ret = ContentAffiliationRetrieval(aff)
             former_aff_dict = {'scopus idx': scopus_search[i], 'aff_name': aff_ret.affiliation_name,
-                               'aff_city': aff_ret.city, 'aff_state': aff_ret.state, 'aff_country': aff_ret_country,
-                               'aff_author_count': aff_ret.author_count, 'aff_document_count': aff_ret.document_count}
+                                   'aff_city': aff_ret.city, 'aff_state': aff_ret.state, 'aff_country': aff_ret.country,
+                                   'aff_author_count': aff_ret.author_count, 'aff_document_count': aff_ret.document_count}
             former_aff_dict_df = pd.DataFrame.from_dict(former_aff_dict)
-            affiliations_df.append(former_aff_dict_df)
+            affiliations_df = affiliations_df.append(former_aff_dict_df)
 
     except:
-        
-        # Create empty Scopus dictionary 
-        empty_scopus_dict = {'scopus idx': scopus_search[i],
-        'name': scopus_search[i][0].title() + ' ' + scopus_search[i][1].title(), 
-        'scopus_id': 'NA','document_count': 'NA', 'citation_count': 'NA',
-        'h_index': 'NA', 'begin_publication_range': 'NA',
-        'end_publication_range': 'NA', 'aff_current_name': 'NA',
-        'aff_current_city': 'NA', 'aff_current_state': 'NA',
-        'aff_current_auth_count': 'NA', 'aff_current_doc_count': 'NA'}     
+        print('empty')
+        # Create empty Scopus dictionary
+        empty_scopus_dict = {'scopus idx': scopus_search[i], 'name': scopus_search[i][0].title() + ' ' + scopus_search[i][1].title(),
+                              'scopus_id': 'NA', 'document_count': 'NA', 'coauthor_count': 'NA', 'citation_count': 'NA',
+                              'h_index': 'NA', 'begin_publication_range': 'NA',
+                              'end_publication_range': 'NA', 'aff_current_name': 'NA',
+                              'aff_current_city': 'NA', 'aff_current_state': 'NA',
+                              'aff_current_auth_count': 'NA', 'aff_current_doc_count': 'NA'}
         
         # Append empty scopus dict to scopus df
-        scopus_dict_df = pd.DataFrame.from_dict(scopus_dict)
-        scopus_df.append(scopus_dict_df)
-
+        empty_scopus_dict_df = pd.DataFrame.from_dict(empty_scopus_dict)
+        scopus_df.append(empty_scopus_dict_df)
 
 scopus_df.to_csv('scopus_data_0_25.csv')
 coauthors_df.to_csv('coauthors_data_0_25.csv')
